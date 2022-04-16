d51def80ccc91859c8d8c9bc668a4b9c1fc5e587
jankenshow <perchon001@gmail.com>
2022-04-17 01:23:41 +0900

    fix to use time_utils

--- src/matmul/matmul_cublas.cu
+++ src/matmul/matmul_cublas.cu
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cublas_v2.h>
 #include "matrix_generator.h"
+#include "time_utils.h"
 
 inline cublasStatus_t gpu_blas_mmul(const float *A, const float *B, float *C, const int m, const int n, const int k, cublasHandle_t* handle) {
     // int lda=m,ldb=k,ldc=m;
@@ -9,7 +10,6 @@ inline cublasStatus_t gpu_blas_mmul(const float *A, const float *B, float *C, co
 
     cublasStatus_t stat;
 
-    // Do the actual multiplication
     stat = cublasSgemm(*handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, &alpha, A, m, B, k, &beta, C, m);
     return stat;
 }
@@ -35,69 +35,60 @@ void test(int size, int iterations) {
     randn_matrices(x, y, x_height, y_width, num_prod);
     zero_matrix(out, num_prod, num_prod);
 
-    double d_sec_memcpy_cuda = 0;
-    double d_sec_kernel_cuda = 0;
-    double d_sec_total_cuda = 0;
+    stopwatch sw;
+    double d_sec_memcpy = 0;
+    double d_sec_kernel = 0;
+    double d_sec_total = 0;
 
     // Create a handle for CUBLAS
     cublasHandle_t handle;
     cublasCreate(&handle);
-    cublasStatus_t stat;
+    // cublasStatus_t stat;
 
     for (int num_iter=0; num_iter<(iterations+1); num_iter++) {
 
-        cudaEvent_t start, memh2d_stop, kernel_stop, memd2h_stop;
-        cudaEventCreate(&start);
-        cudaEventCreate(&memh2d_stop);
-        cudaEventCreate(&kernel_stop);
-        cudaEventCreate(&memd2h_stop);
-        cudaEventRecord(start);
+        if (num_iter != 0) {
+            sw.start();
+        }
 
-        // Allocate 3 arrays on GPU
         float *d_x, *d_y, *d_out;
         cudaMalloc(&d_x, x_height * num_prod * sizeof(float));
         cudaMalloc(&d_y, num_prod * y_width * sizeof(float));
         cudaMalloc(&d_out, num_prod * num_prod * sizeof(float));
-        
-        // Optionally we can copy the data back on CPU and print the arrays
+
         cudaMemcpy(d_x, x, x_height * num_prod  * sizeof(float), cudaMemcpyHostToDevice);
         cudaMemcpy(d_y, y, num_prod * y_width * sizeof(float), cudaMemcpyHostToDevice);
 
-        cudaEventRecord(memh2d_stop);
-        cudaEventSynchronize(memh2d_stop);
+        if (num_iter != 0) {
+            cudaDeviceSynchronize();
+            sw.lap();
+        }
 
-        // Multiply A and B on GPU
-        stat = gpu_blas_mmul(d_x, d_y, d_out, x_height, y_width, num_prod, &handle);
-
-        cudaEventRecord(kernel_stop);
-        cudaEventSynchronize(kernel_stop);
-
-        // Copy (and print) the result on host memory
-        cudaMemcpy(out, d_out, x_height * y_width * sizeof(float), cudaMemcpyDeviceToHost);
-
-        cudaEventRecord(memd2h_stop);
-        cudaEventSynchronize(memd2h_stop);
+        gpu_blas_mmul(d_x, d_y, d_out, x_height, y_width, num_prod, &handle);
+        // stat = gpu_blas_mmul(d_x, d_y, d_out, x_height, y_width, num_prod, &handle);
 
         if (num_iter != 0) {
-            float milisec = 0.0;
-            cudaEventElapsedTime(&milisec, start, memd2h_stop);
-            d_sec_total_cuda += milisec;
-            cudaEventElapsedTime(&milisec, start, memh2d_stop);
-            d_sec_memcpy_cuda += milisec;
-            cudaEventElapsedTime(&milisec, memh2d_stop, kernel_stop);
-            d_sec_kernel_cuda += milisec;
+            cudaDeviceSynchronize();
+            sw.lap();
         }
 
-        if (stat != CUBLAS_STATUS_SUCCESS) {
-            printf ("CUBLAS failed\n");
+        cudaMemcpy(out, d_out, x_height * y_width * sizeof(float), cudaMemcpyDeviceToHost);
+
+        // 時間計測
+        if (num_iter != 0) {
+            cudaDeviceSynchronize();
+            sw.stop();
+            d_sec_memcpy += sw.get_lap(0);
+            d_sec_kernel += sw.get_lap(1);
+            d_sec_total += sw.get_total();
         }
 
-        cudaEventDestroy(start);
-        cudaEventDestroy(memh2d_stop);
-        cudaEventDestroy(kernel_stop);
-        cudaEventDestroy(memd2h_stop);
+        // if (stat != CUBLAS_STATUS_SUCCESS) {
+        //     printf ("CUBLAS failed\n");
+        // }
 
-        //Free GPU memory
+        sw.reset();
+
         cudaFree(d_x);
         cudaFree(d_y);
         cudaFree(d_out);
@@ -107,17 +98,16 @@ void test(int size, int iterations) {
     cublasDestroy(handle);
     cudaDeviceReset();
 
-    d_sec_memcpy_cuda /= iterations;
-    d_sec_kernel_cuda /= iterations;
-    d_sec_total_cuda /= iterations;
+    d_sec_memcpy /= iterations;
+    d_sec_kernel /= iterations;
+    d_sec_total /= iterations;
 
     printf("行列サイズ=%d\n", size);
     printf("計算結果=%f\n", out[x_height * y_width - 1]);
-    printf("処理時間 : メモリ移動 = %lf\n", d_sec_memcpy_cuda);
-    printf("処理時間 : カーネル実行 =%lf\n", d_sec_kernel_cuda);
-    printf("処理時間 : トータル =%lf\n", d_sec_total_cuda);
+    printf("処理時間 : メモリ移動 = %lf\n", d_sec_memcpy);
+    printf("処理時間 : カーネル実行 =%lf\n", d_sec_kernel);
+    printf("処理時間 : トータル =%lf\n", d_sec_total);
 
-    // Free CPU memory
     free(x);
     free(y);
     free(out);
--- src/matmul/matmul_cuda.cu
+++ src/matmul/matmul_cuda.cu
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <sys/time.h>
+#include <string>
 #include <random>
 #include <cmath>
+#include "matrix_generator.h"
+#include "time_utils.h"
 
 
 __global__ void matmul(float* out, float* x, float* y, int height, int width, int num_prod) {
@@ -16,54 +19,43 @@ __global__ void matmul(float* out, float* x, float* y, int height, int width, in
     }
 }
 
-__host__ inline void randn_matrices(float* x, float* y, int x_height, int y_width, int num_prod) {
-    std::random_device rnd;
-    std::default_random_engine eng(rnd());
-    std::uniform_real_distribution<float> distr(-1, 1);
-
-    for (int i=0; i < x_height * num_prod; i++) {
-        x[i] = distr(eng);
-    }
-
-    for (int i=0; i < num_prod * y_width; i++) {
-        y[i] = distr(eng);
-    }
-}
-
-void test(int size, int iterations) {
+void test(int size, int iterations, int thread_size) {
     int x_height = size;
     int num_prod = size;
     int y_width = size;
-
-    int thread_size = 32;
 
     float* x = new float[x_height * num_prod];
     float* y = new float[num_prod * y_width];
     float* out = new float[x_height * y_width];
     randn_matrices(x, y, x_height, y_width, num_prod);
 
-    struct timespec start_time, memcpy_end_time, kernel_end_time, total_end_time;
-    unsigned int sec;
-    int nsec;
+    dim3 grid(std::ceil(x_height / thread_size), std::ceil(y_width / thread_size), 1);
+    dim3 threads(thread_size, thread_size, 1);
+    printf("grid_size=(%d, %d, %d)\n", grid.x, grid.y, grid.z);
+    printf("thread_size=(%d, %d, %d)\n", threads.x, threads.y, threads.z);
+
+    stopwatch sw;
     double d_sec_memcpy = 0;
     double d_sec_kernel = 0;
     double d_sec_total = 0;
 
-    double d_sec_memcpy_cuda = 0;
-    double d_sec_kernel_cuda = 0;
-    double d_sec_total_cuda = 0;
+    // double d_sec_memcpy_cuda = 0;
+    // double d_sec_kernel_cuda = 0;
+    // double d_sec_total_cuda = 0;
 
     for (int num_iter=0; num_iter<(iterations+1); num_iter++) {
 
         // 時間計測
-        clock_gettime(CLOCK_REALTIME, &start_time);
+        if (num_iter != 0) {
+            sw.start();
+        }
 
-        cudaEvent_t start, memh2d_stop, kernel_stop, memd2h_stop;
-        cudaEventCreate(&start);
-        cudaEventCreate(&memh2d_stop);
-        cudaEventCreate(&kernel_stop);
-        cudaEventCreate(&memd2h_stop);
-        cudaEventRecord(start);
+        // cudaEvent_t start, memh2d_stop, kernel_stop, memd2h_stop;
+        // cudaEventCreate(&start);
+        // cudaEventCreate(&memh2d_stop);
+        // cudaEventCreate(&kernel_stop);
+        // cudaEventCreate(&memd2h_stop);
+        // cudaEventRecord(start);
 
 
         // メモリ移動 host -> device
@@ -82,34 +74,25 @@ void test(int size, int iterations) {
         // 時間計測
         if (num_iter != 0) {
             cudaDeviceSynchronize();
-            clock_gettime(CLOCK_REALTIME, &memcpy_end_time);
-            sec = memcpy_end_time.tv_sec - start_time.tv_sec;
-            nsec = memcpy_end_time.tv_nsec - start_time.tv_nsec;
-            d_sec_memcpy += (double)sec * 1000 + (double)nsec / (1000 * 1000);
+            sw.lap();
         }
 
-        cudaEventRecord(memh2d_stop);
-        cudaEventSynchronize(memh2d_stop);
+        // cudaEventRecord(memh2d_stop);
+        // cudaEventSynchronize(memh2d_stop);
 
 
         // kernel実行
-        dim3 grid(std::ceil(x_height / thread_size), std::ceil(y_width / thread_size), 1);
-        dim3 threads(thread_size, thread_size, 1);
-
         matmul<<<grid, threads>>>(d_out, d_x, d_y, x_height, y_width, num_prod);
 
 
         // 時間計測
         if (num_iter != 0) {
             cudaDeviceSynchronize();
-            clock_gettime(CLOCK_REALTIME, &kernel_end_time);
-            sec = kernel_end_time.tv_sec - memcpy_end_time.tv_sec;
-            nsec = kernel_end_time.tv_nsec - memcpy_end_time.tv_nsec;
-            d_sec_kernel += (double)sec * 1000 + (double)nsec / (1000 * 1000);
+            sw.lap();
         }
 
-        cudaEventRecord(kernel_stop);
-        cudaEventSynchronize(kernel_stop);
+        // cudaEventRecord(kernel_stop);
+        // cudaEventSynchronize(kernel_stop);
 
 
         // メモリ移動 device -> host
@@ -119,30 +102,32 @@ void test(int size, int iterations) {
         // 時間計測
         if (num_iter != 0) {
             cudaDeviceSynchronize();
-            clock_gettime(CLOCK_REALTIME, &total_end_time);
-            sec = total_end_time.tv_sec - start_time.tv_sec;
-            nsec = total_end_time.tv_nsec - start_time.tv_nsec;
-            d_sec_total += (double)sec * 1000 + (double)nsec / (1000 * 1000);
+            sw.stop();
+            d_sec_memcpy += sw.get_lap(0);
+            d_sec_kernel += sw.get_lap(1);
+            d_sec_total += sw.get_total();
         }
 
-        cudaEventRecord(memd2h_stop);
-        cudaEventSynchronize(memd2h_stop);
+        // cudaEventRecord(memd2h_stop);
+        // cudaEventSynchronize(memd2h_stop);
 
-        if (num_iter != 0) {
-            float milisec = 0.0;
-            cudaEventElapsedTime(&milisec, start, memd2h_stop);
-            d_sec_total_cuda += milisec;
-            cudaEventElapsedTime(&milisec, start, memh2d_stop);
-            d_sec_memcpy_cuda += milisec;
-            cudaEventElapsedTime(&milisec, memh2d_stop, kernel_stop);
-            d_sec_kernel_cuda += milisec;
-        }
+        // if (num_iter != 0) {
+        //     float milisec = 0.0;
+        //     cudaEventElapsedTime(&milisec, start, memd2h_stop);
+        //     d_sec_total_cuda += milisec;
+        //     cudaEventElapsedTime(&milisec, start, memh2d_stop);
+        //     d_sec_memcpy_cuda += milisec;
+        //     cudaEventElapsedTime(&milisec, memh2d_stop, kernel_stop);
+        //     d_sec_kernel_cuda += milisec;
+        // }
+
 
         // 時間計測後処理
-        cudaEventDestroy(start);
-        cudaEventDestroy(memh2d_stop);
-        cudaEventDestroy(kernel_stop);
-        cudaEventDestroy(memd2h_stop);
+        sw.reset();
+        // cudaEventDestroy(start);
+        // cudaEventDestroy(memh2d_stop);
+        // cudaEventDestroy(kernel_stop);
+        // cudaEventDestroy(memd2h_stop);
 
 
         // kernel実行後処理
@@ -159,15 +144,18 @@ void test(int size, int iterations) {
     d_sec_kernel /= iterations;
     d_sec_total /= iterations;
 
-    d_sec_memcpy_cuda /= iterations;
-    d_sec_kernel_cuda /= iterations;
-    d_sec_total_cuda /= iterations;
+    // d_sec_memcpy_cuda /= iterations;
+    // d_sec_kernel_cuda /= iterations;
+    // d_sec_total_cuda /= iterations;
 
     printf("行列サイズ=%d\n", size);
     printf("計算結果=%f\n", out[x_height * y_width - 1]);
-    printf("処理時間 : メモリ移動 = %lf vs %lf\n", d_sec_memcpy, d_sec_memcpy_cuda);
-    printf("処理時間 : カーネル実行 =%lf vs %lf\n", d_sec_kernel, d_sec_kernel_cuda);
-    printf("処理時間 : トータル =%lf vs %lf\n", d_sec_total, d_sec_total_cuda);
+    printf("処理時間 : メモリ移動 = %lf\n", d_sec_memcpy);
+    printf("処理時間 : カーネル実行 =%lf\n", d_sec_kernel);
+    printf("処理時間 : トータル =%lf\n", d_sec_total);
+    // printf("処理時間 : メモリ移動 = %lf vs %lf\n", d_sec_memcpy, d_sec_memcpy_cuda);
+    // printf("処理時間 : カーネル実行 =%lf vs %lf\n", d_sec_kernel, d_sec_kernel_cuda);
+    // printf("処理時間 : トータル =%lf vs %lf\n", d_sec_total, d_sec_total_cuda);
 
     delete[] x;
     delete[] y;
@@ -175,12 +163,15 @@ void test(int size, int iterations) {
 }
 
 
-int main() {
+int main(int argc, char** argv) {
     int iterations = 10;
     int sizes[3] = {256, 1024, 4096};
 
+    std::size_t pos;
+    int thread_size = std::stoi((std::string)argv[1], &pos);
+
     for (auto& size : sizes) {
-        test(size, iterations);
+        test(size, iterations, thread_size);
     }
 
     return 0;
