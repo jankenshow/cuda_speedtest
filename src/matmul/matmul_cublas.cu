2918015e5565520756bd79efa6ff2e00263573ca
jankenshow <perchon001@gmail.com>
2022-04-16 12:16:14 +0900

    bugfix

--- src/matmul/matmul_cublas.cu
+++ src/matmul/matmul_cublas.cu
@@ -1,42 +1,18 @@
-// #include <cstdlib>
 #include <stdio.h>
-#include <random>
-#include <cmath>
 #include <cublas_v2.h>
+#include "matrix_generator.h"
 
+inline cublasStatus_t gpu_blas_mmul(const float *A, const float *B, float *C, const int m, const int n, const int k, cublasHandle_t* handle) {
+    // int lda=m,ldb=k,ldc=m;
+    const float alpha = 1.0f;
+    const float beta = 0.0f;
 
-__host__ inline void randn_matrices (float* x, float* y, int x_height, int y_width, int num_prod) {
-    std::random_device rnd;
-    std::default_random_engine eng(rnd());
-    std::uniform_real_distribution<float> distr(-1, 1);
-
-    for (int i=0; i < x_height * num_prod; i++) {
-        x[i] = distr(eng);
-    }
-
-    for (int i=0; i < num_prod * y_width; i++) {
-        y[i] = distr(eng);
-    }
-}
-
-inline void gpu_blas_mmul(const float *A, const float *B, float *C, const int m, const int n, const int k) {
-    int lda=m,ldb=k,ldc=m;
-    const float alf = 1;
-    const float bet = 0;
-    const float *alpha = &alf;
-    const float *beta = &bet;
-
-    // Create a handle for CUBLAS
-    cublasHandle_t handle;
-    cublasCreate(&handle);
+    cublasStatus_t stat;
 
     // Do the actual multiplication
-    cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
-
-    // Destroy the handle
-    cublasDestroy(handle);
+    stat = cublasSgemm(*handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, &alpha, A, m, B, k, &beta, C, m);
+    return stat;
 }
-
 
 void test(int size, int iterations) {
     // Allocate 3 arrays on CPU
@@ -44,16 +20,32 @@ void test(int size, int iterations) {
     int num_prod = size;
     int y_width = size;
 
+    // // setup execution parameters
+    // cudaSetDevice(0);
+    // cudaDeviceProp deviceProp;
+    // cudaGetDeviceProperties(&deviceProp, 0);
+    // int block_size = (deviceProp.major < 2) ? 16 : 32;
+    // dim3 threads(block_size, block_size);
+    // dim3 grid(size / threads.x, size / threads.y);
+    // printf("block_size=%d, grid_size=%d\n", block_size, size/threads.x);
+
     float *x = (float *)malloc(x_height * num_prod * sizeof(float));
     float *y = (float *)malloc(num_prod * y_width * sizeof(float));
     float *out = (float *)malloc(num_prod * num_prod * sizeof(float));
     randn_matrices(x, y, x_height, y_width, num_prod);
+    zero_matrix(out, num_prod, num_prod);
 
     double d_sec_memcpy_cuda = 0;
     double d_sec_kernel_cuda = 0;
     double d_sec_total_cuda = 0;
 
+    // Create a handle for CUBLAS
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+    cublasStatus_t stat;
+
     for (int num_iter=0; num_iter<(iterations+1); num_iter++) {
+
         cudaEvent_t start, memh2d_stop, kernel_stop, memd2h_stop;
         cudaEventCreate(&start);
         cudaEventCreate(&memh2d_stop);
@@ -68,20 +60,20 @@ void test(int size, int iterations) {
         cudaMalloc(&d_out, num_prod * num_prod * sizeof(float));
         
         // Optionally we can copy the data back on CPU and print the arrays
-        cudaMemcpy(x, d_x, x_height * num_prod  * sizeof(float), cudaMemcpyDeviceToHost);
-        cudaMemcpy(y, d_y, num_prod * y_width * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(d_x, x, x_height * num_prod  * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_y, y, num_prod * y_width * sizeof(float), cudaMemcpyHostToDevice);
 
         cudaEventRecord(memh2d_stop);
         cudaEventSynchronize(memh2d_stop);
 
         // Multiply A and B on GPU
-        gpu_blas_mmul(d_x, d_y, d_out, x_height, y_width, num_prod);
+        stat = gpu_blas_mmul(d_x, d_y, d_out, x_height, y_width, num_prod, &handle);
 
         cudaEventRecord(kernel_stop);
         cudaEventSynchronize(kernel_stop);
 
         // Copy (and print) the result on host memory
-        cudaMemcpy(out, d_out, num_prod * num_prod * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(out, d_out, x_height * y_width * sizeof(float), cudaMemcpyDeviceToHost);
 
         cudaEventRecord(memd2h_stop);
         cudaEventSynchronize(memd2h_stop);
@@ -89,11 +81,15 @@ void test(int size, int iterations) {
         if (num_iter != 0) {
             float milisec = 0.0;
             cudaEventElapsedTime(&milisec, start, memd2h_stop);
-            d_sec_total_cuda += milisec / 1000;
+            d_sec_total_cuda += milisec;
             cudaEventElapsedTime(&milisec, start, memh2d_stop);
-            d_sec_memcpy_cuda += milisec / 1000;
+            d_sec_memcpy_cuda += milisec;
             cudaEventElapsedTime(&milisec, memh2d_stop, kernel_stop);
-            d_sec_kernel_cuda += milisec / 1000;
+            d_sec_kernel_cuda += milisec;
+        }
+
+        if (stat != CUBLAS_STATUS_SUCCESS) {
+            printf ("CUBLAS failed\n");
         }
 
         cudaEventDestroy(start);
@@ -107,6 +103,8 @@ void test(int size, int iterations) {
         cudaFree(d_out);
     }
 
+    // Destroy the handle
+    cublasDestroy(handle);
     cudaDeviceReset();
 
     d_sec_memcpy_cuda /= iterations;
